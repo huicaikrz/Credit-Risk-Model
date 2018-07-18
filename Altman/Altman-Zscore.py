77878f8aff66ac7e050098b7305dfb1c082f2ed3
huicaikrz <31255169+huicaikrz@users.noreply.github.com>
2018-07-18 11:03:52 +0800

    update

--- Altman/Altman-Zscore.py
+++ Altman/Altman-Zscore.py
@@ -34,18 +34,12 @@ income = pd.DataFrame(list(cur.fetchall()),columns = ['WIND_CODE','ANN_DT',
 bal = bal.set_index([bal['WIND_CODE'],bal['REPORT_PERIOD']])
 income = income.set_index([income['WIND_CODE'],income['REPORT_PERIOD']])
 
-#emerge Z = 3.25 + 6.56X1 + 3.26X2 + 6.72X3 + 1.05X4
-#Z = 1.2X1 + 1.4X2 + 3.3X3 + 0.6X4 + 0.999X5
-#把所有的都调出来，然后存储
+#emerging market formula Z = 3.25 + 6.56X1 + 3.26X2 + 6.72X3 + 1.05X4
 
-#遍历每一天
-#所有的股票算一个z-score
-
-#join two dataframe accoeding to the index
+#join two dataframe (balance sheet and income statement) according to the index
 data = pd.merge(bal,income)
 data[data.columns[3:]] = data[data.columns[3:]].astype('float')
 data = data.dropna()
-#X1:(current assets - current liabilities)/total assets
 
 x1 = (data['TOT_CUR_ASSETS'] - data['TOT_CUR_LIAB'])/data['TOT_ASSETS']
 x2 = (data['S_FA_RETAINEDEARNINGS']/data['TOT_ASSETS'])
@@ -58,15 +52,9 @@ z_score['WIND_CODE'] = data['WIND_CODE'];z_score['REPORT_PERIOD'] = data['REPORT
 z_score= z_score.set_index([data['WIND_CODE'],data['REPORT_PERIOD']])
 z_score = z_score.dropna()
 
+#threshold, <1.1, 1.1-2.6, >2.6
 bankruptcy = z_score[z_score['z'] < 1.1].dropna()
-#<1.1,1.1-2.6,>2.6
-
-des = bankruptcy.groupby(by='WIND_CODE')
 grey = z_score[(z_score < 2.6)&(z_score > 1.1)].dropna()
-g = grey.groupby(by = 'WIND_CODE')
-
-
-#自动update
 
 
 
